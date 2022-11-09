eebfe0cbdb18ef513f3b1de171da6ad8dcabf227
WeslleySOR <weslleywruas@gmail.com>
2022-11-09 17:14:28 -0300

    ficticio craft created

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Fictício Craft - Home</title>
+        <meta name="description" content="Página inicial do fictício craft." />
+      </Head>
+      <main>
+        <h1>HomePage</h1>
+      </main>
+    </div>
+  )
+}
